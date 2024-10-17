7805487e49ff05bbe060e1eaf5d0fec91b175986
giorgiodallaglio <36376022+giorgiodallaglio@users.noreply.github.com>
2024-10-18 01:40:48 +0200

    Update log.php

--- log.php
+++ log.php
@@ -1,11 +1,13 @@
 <?php
-    $logData = $_POST['logData'];
-    $logFile = 'log.txt';
+    if (isset($_POST['logData'])) {
+        $logData = $_POST['logData'];
+        $logFile = 'log.txt';
 
-    // Aggiungi la data e ora ai dati di log
-    $timestamp = date("Y-m-d H:i:s");
-    $logData = $timestamp . " - " . $logData . "\n";
+        // Aggiungi la data e ora ai dati di log
+        $timestamp = date("Y-m-d H:i:s");
+        $logData = $timestamp . " - " . $logData . "\n";
 
-    // Salva i dati nel file log.txt
-    file_put_contents($logFile, $logData, FILE_APPEND);
+        // Salva i dati nel file log.txt
+        file_put_contents($logFile, $logData, FILE_APPEND);
+    }
 ?>
