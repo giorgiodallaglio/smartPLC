63237aa884912a085b07176707d48488fdbc680a
giorgiodallaglio <36376022+giorgiodallaglio@users.noreply.github.com>
2024-10-18 01:37:25 +0200

    Create log.php

---
+++ log.php
@@ -0,0 +1,11 @@
+<?php
+    $logData = $_POST['logData'];
+    $logFile = 'log.txt';
+
+    // Aggiungi la data e ora ai dati di log
+    $timestamp = date("Y-m-d H:i:s");
+    $logData = $timestamp . " - " . $logData . "\n";
+
+    // Salva i dati nel file log.txt
+    file_put_contents($logFile, $logData, FILE_APPEND);
+?>
